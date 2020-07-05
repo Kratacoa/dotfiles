72c1247cd54ee85967af9d5089d4d3a9b4a2e311
Kratacoa <kratacoa@protonmail.com>
2020-07-05 02:38:19 +0200

    Vi-like copypaste in tmux; fix fish, xinitrc, bspwmrc, tmux.conf location

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,14 +1,15 @@
-set --export EDITOR nvim
+set --export EDITOR /bin/kak
 set --export BROWSER /usr/bin/firefox
-set CDPATH .:~:~/books
+# set -a --export PATH :/home/splop/code/scripts/
 
 # Syntax highlighting for less
-export LESS="-R "
-export LESSOPEN="| /usr/bin/src-hilite-lesspipe.sh %s"
+set --export LESS "-R "
+set --export LESSOPEN "| /usr/bin/src-hilite-lesspipe.sh %s"
+set --export LESSHISTFILE "~/.local/share/less/lesshst"
 
 # Start X at login
 if status is-login
     if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
-				exec startx -- -keeptty
-		end
+	exec startx -- -keeptty
+    end
 end
