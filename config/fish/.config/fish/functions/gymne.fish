362e209a71e137f8d4eab96c93e4e9db1e434631
Kratacoa <kratacoa@protonmail.com>
2021-01-21 11:58:16 +0000

    fish: added medne and gymne, for respective logs

---
+++ config/fish/.config/fish/functions/gymne.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function gymne --wraps='kak -e gymnastics-new' --description 'alias gymne=kak -e gymnastics-new'
+  kak -e gymnastics-new $argv;
+end
---
+++ config/fish/.config/fish/functions/medne.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function medne --wraps='kak -e meditation-new' --description 'alias medne=kak -e meditation-new'
+  kak -e meditation-new $argv;
+end
