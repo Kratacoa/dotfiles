d3c39deba6d09c094f78deb31f3fa43439de5f1b
Kratacoa <kratacoa@protonmail.com>
2021-05-20 12:18:00 +0300

    fish: fixed source-highlight

--- config/fish/.config/fish/config.fish
+++ config/fish/.config/fish/config.fish
@@ -27,13 +27,6 @@ set --export FZF_DEFAULT_OPTS "--cycle --color=light"
 
 # less configuration: setting the history file, highlighting and some other stuff
 set --export LESSHISTFILE "~/.local/share/less/lesshst"
-set --export LESS_TERMCAP_mb (printf "\e[1;31m")	# begin blink
-set --export LESS_TERMCAP_md (printf "\e[1;36m")	# begin bold
-set --export LESS_TERMCAP_me (printf "\e[0m")	#reset bold/blink
-set --export LESS_TERMCAP_so (printf "\e[01;44;33m")	# begin reverse video
-set --export LESS_TERMCAP_se (printf "\e[0m")	# reset reverse video
-set --export LESS_TERMCAP_us (printf "\e[1;32m") # begin underline
-set --export LESS_TERMCAP_ue (printf "\e[0m")	# reset underline
 set --export LESS " --ignore-case --status-column --tabs=4 --LONG-PROMPT --RAW-CONTROL-CHARS --HILITE-UNREAD"
 set --export LESSOPEN "| /usr/bin/src-hilite-lesspipe.sh %s"
 
